bcd87f72d52f09eac67522d8b24e04234ffa870e
Nova Merida <novareymerida@gmail.com>
2025-05-26 21:40:48 -0400

    this is my learning for java

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args){
+
+        /* 
+        System.out.print("Hello World! ");
+        System.out.print("My name is Nova ");
+        System.out.println("I love learning Java. ");
+        // We are taking our salary and seeing what we would make every month of the year
+        System.out.println(80000 / 12);
+        */
+        // final int myAge = 21;   "final" = constant var type
+        // myAge = 20;
+        /*System.out.println(name);
+        System.out.println(myAge);*/
+
+        /*
+        final String name = "Nova Merida";
+        int myAge = 21;
+        float myAgeDogYears = 35.7f;
+        System.out.println("Name: " + name + " Age: " + myAge + " DogYearsAge: " + myAgeDogYears );
+        */
+        /*
+        String firstName = "Nova ";
+        String lastName = "Merida ";
+        int myAge = 20;
+        String bio = firstName + lastName + myAge;
+        System.out.println(bio);
+        */
+        // Multiple variable one line
+        /*
+        int x = 5, y = 10, z = -20;
+        System.out.println(x + y + z);
+        */
+
+}}
